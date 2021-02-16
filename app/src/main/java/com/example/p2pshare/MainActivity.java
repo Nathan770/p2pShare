9cc7c5cebc830f3accdeb4127a3b6db124b57b90
nathan amiel <nanathanamiel5@gmail.com>
2021-02-16 09:51:21 +0200

    fix bug

--- app/src/main/java/com/example/p2pshare/MainActivity.java
+++ app/src/main/java/com/example/p2pshare/MainActivity.java
@@ -11,12 +11,14 @@ import com.example.p2psharelibrary.p2pClass;
 
 public class MainActivity extends AppCompatActivity implements ConnectionSatutsCallBack {
     private static final String TAG = "nathan";
+    private p2pClass p2pClass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        p2pClass p2pClass = new p2pClass(this);
+        p2pClass = new p2pClass(this);
         p2pClass.run();
         p2pClass.searchClient();
 
@@ -26,17 +28,23 @@ public class MainActivity extends AppCompatActivity implements ConnectionSatutsC
     public void connectionStatus(boolean connected) {
         if (connected){
             Log.d(TAG, "connectionStatus: connected");
+
+            String ms = "hello nathan";
+            p2pClass.sendToClient(ms.getBytes());
+
         }else
             Log.d(TAG, "connectionStatus: no connected");
     }
 
     @Override
     public void getPeer(WifiP2pDevice[] wifiP2pDevice) {
-        Log.d(TAG, "getPeer: before" +wifiP2pDevice.toString());
+        Log.d(TAG, "getPeer: before" +wifiP2pDevice.length);
         if (wifiP2pDevice != null){
             for (int i = 0; i < wifiP2pDevice.length; i++) {
                 Log.d(TAG, "getPeer: "+wifiP2pDevice[i].deviceName);
             }
+
+//            p2pClass.connectToClient(wifiP2pDevice[0]);
         }
 
     }
