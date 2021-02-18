ca81cf3e31b9e9d72ade561b61d06315d004b36f
nathan amiel <nanathanamiel5@gmail.com>
2021-02-18 19:16:55 +0200

    finish

--- app/src/main/java/com/example/p2pshare/MainActivity.java
+++ app/src/main/java/com/example/p2pshare/MainActivity.java
@@ -154,6 +154,7 @@ public class MainActivity extends AppCompatActivity implements ConnectionSatutsC
     public void messageReciever(byte[] arr,int arg) {
         String msg = new String(arr, 0, arg);
         main2_TXT_message.setText(msg);
+
         main2_TXT_message.setVisibility(View.VISIBLE);
     }
 
